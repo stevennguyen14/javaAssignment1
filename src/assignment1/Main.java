469e9b3420cdc0019f52ea178782e5e22c3e070f
steven <steven.101@live.com.au>
2018-04-30 11:05:15 +1000

    moved webbased in main to function

--- src/assignment1/Main.java
+++ src/assignment1/Main.java
@@ -24,7 +24,7 @@ public class Main {
      Boolean exit = false;
      String[] choice = {"ATM", "WebBased", "Exit"};
      String[] accountChoice = {"Savings Account", "Net Savings Account", "Cheque Account", "Fixed Account"};
-     String[] webBasedOption = {"Set Withdraw Limit", "Transfer funds between accounts", "Calculate interest"};
+     
      
      while (!exit){
      
@@ -61,143 +61,9 @@ public class Main {
         }
         //if select web based interface
         else if(input == choice[1]){
-            //create web based instance
-            WebBased webBased = new WebBased();
-            //notify user web based has been selected
-            JOptionPane.showMessageDialog(null, "Web Based interface has been selected");
-            
-            //ask user to select which account
-            String accountType = (String) JOptionPane.showInputDialog(null, "Please select your account", "Select Account",
-                    JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice[0]);
-            //select what the user would like to do with the account?
-            //savings account
-            if(accountType == accountChoice[0]){
-                String webBasedChoice = (String) JOptionPane.showInputDialog(null, "Would you like to set withdraw limit or transfer funds to another account or calculate interest?",
-                        "Select Option", JOptionPane.QUESTION_MESSAGE, null, webBasedOption, webBasedOption[0]);
-                //if choice is set withdraw limit         
-                if(webBasedChoice == webBasedOption[0]){
-                    //ask user to enter withdraw limit
-                    double withdrawLimit = Double.parseDouble(JOptionPane.showInputDialog("Please enter the new withdraw limit"));
-                    //set new withdraw limit
-                    savingsAccount.setWithdrawLimit(withdrawLimit);
-                }
-                //if choice is to transfer funds
-                else if(webBasedChoice == webBasedOption[1]){
-                    String account = (String) JOptionPane.showInputDialog(null, "Which account do you want to transfer funds to?",
-                        "Select option", JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice [1]);
-                    //if trying to transfer to same account, throw error
-                    if(account == accountChoice[0]){
-                        JOptionPane.showMessageDialog(null, "You cannot transfer to the same account");
-                    }
-                    //net savings account
-                    else if(account == accountChoice[1]){
-                        webBased.transferBalance(savingsAccount, netSavingsAccount);
-                    }
-                    //cheque account
-                    else if(account == accountChoice[2]){
-                        webBased.transferBalance(savingsAccount, chequeAccount);
-                    }
-                    //fixed account
-                    else if (account == accountChoice[3]){
-                        webBased.transferBalance(savingsAccount, fixedAccount);
-                    } 
-                }
-                //if choice is calculate interest earned
-                else if (webBasedChoice == webBasedOption[2]){
-                    //calculate savings account interest
-                    savingsAccount.calculateInterestEarned();
-                }
+           WebBased webBased = new WebBased(); 
+            webBased.runWebBased(savingsAccount, netSavingsAccount, chequeAccount, fixedAccount);
             }
-            //if account choice is net savings account
-            else if(accountType == accountChoice[1]){
-                //
-                String webBasedChoice = (String) JOptionPane.showInputDialog(null, "transfer funds to another account or calculate interest?",
-                        "Select Option", JOptionPane.QUESTION_MESSAGE, null, webBasedOption, webBasedOption[0]);
-                //net savings account cant set withdraw limit, so give error message
-                if(webBasedChoice == webBasedOption[0]){
-                    JOptionPane.showMessageDialog(null, "You cannot set withdraw limit for net savings account");
-                }
-                else if(webBasedChoice == webBasedOption[1]){
-                //ask user which account to send funds to
-                String account = (String) JOptionPane.showInputDialog(null, "Which account do you want to transfer funds to?",
-                        "Select option", JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice [0]);
-                    //if account to transfer to is savings account
-                    if(account == accountChoice[0]){
-                       webBased.transferBalance(netSavingsAccount, savingsAccount); 
-                    }
-                    // if same account chosen then send error
-                    else if(account == accountChoice[1]){
-                        JOptionPane.showMessageDialog(null, "You cannot transfer to the same account");
-                    }
-                    //if account = cheque account
-                    else if(account == accountChoice[2]){
-                        webBased.transferBalance(netSavingsAccount, chequeAccount);
-                    }
-                    //fixed account
-                    else if (account == accountChoice[3]){
-                        webBased.transferBalance(savingsAccount, fixedAccount);
-                    } 
-                }
-                //else calculate interest
-                else 
-                    netSavingsAccount.calculateInterestEarned();
-            }
-            //cheque account
-            else if (accountType == accountChoice[2]){
-                String webBasedChoice = (String) JOptionPane.showInputDialog(null, "transfer funds to another account or calculate interest?",
-                        "Select Option", JOptionPane.QUESTION_MESSAGE, null, webBasedOption, webBasedOption[0]);
-                if(webBasedChoice == webBasedOption[0]){
-                    JOptionPane.showMessageDialog(null, "You cannot set withdraw limit for cheque account");
-                }
-                if(webBasedChoice == webBasedOption[1]){
-                String account = (String) JOptionPane.showInputDialog(null, "Which account do you want to transfer funds to?",
-                        "Select option", JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice [0]);
-                    if(account == accountChoice[0]){
-                       webBased.transferBalance(chequeAccount, savingsAccount); 
-                    }
-                    else if(account == accountChoice[1]){
-                       webBased.transferBalance(chequeAccount, netSavingsAccount);
-                    }
-                    else if(account == accountChoice[2]){
-                        JOptionPane.showMessageDialog(null, "You cannot transfer to the same account");
-                    }
-                    else if (account == accountChoice[3]){
-                        webBased.transferBalance(chequeAccount, fixedAccount);
-                    } 
-                }
-                if(webBasedChoice == webBasedOption[2]){
-                    JOptionPane.showMessageDialog(null, "cheque account has no interest");
-                }
-            }
-            else if(accountType == accountChoice[3]){
-                
-                String webBasedChoice = (String) JOptionPane.showInputDialog(null, "transfer funds to another account or calculate interest?",
-                        "Select Option", JOptionPane.QUESTION_MESSAGE, null, webBasedOption, webBasedOption[0]);
-                
-                String account = (String) JOptionPane.showInputDialog(null, "Which account do you want to transfer funds to?",
-                        "Select option", JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice [0]);
-                
-                if(webBasedChoice == webBasedOption[0]){
-                    JOptionPane.showMessageDialog(null, "Fixed account doesn't offer a withdraw limit");
-                }
-                else if(webBasedChoice == webBasedOption[1]){
-                    if(account == accountChoice[0]){
-                       webBased.transferBalance(fixedAccount, savingsAccount); 
-                    }
-                    else if(account == accountChoice[1]){
-                       webBased.transferBalance(fixedAccount, netSavingsAccount);
-                    }
-                    else if(account == accountChoice[2]){
-                       webBased.transferBalance(fixedAccount, chequeAccount);
-                    }
-                    else if (account == accountChoice[3]){
-                        JOptionPane.showMessageDialog(null, "You cannot transfer to the same account");
-                    } 
-                }
-                else
-                    fixedAccount.calculateInterestEarned();
-            }
-        }
         else if(input == choice[2]){
             exit = true;
         }
--- src/assignment1/WebBased.java
+++ src/assignment1/WebBased.java
@@ -24,5 +24,145 @@ public class WebBased extends Account{
         JOptionPane.showMessageDialog(null, "New balance for first account is " + account1.getAccountBalance());
         JOptionPane.showMessageDialog(null, "New balance for second account is " + account2.getAccountBalance());
     }
+    
+    public void runWebBased(SavingsAccount savingsAccount, NetSavingsAccount netSavingsAccount, ChequeAccount chequeAccount, FixedAccount fixedAccount){
+        String[] accountChoice = {"Savings Account", "Net Savings Account", "Cheque Account", "Fixed Account"};
+        String[] webBasedOption = {"Set Withdraw Limit", "Transfer funds between accounts", "Calculate interest"};
+        
+            //notify user web based has been selected
+            JOptionPane.showMessageDialog(null, "Web Based interface has been selected");
+            
+            //ask user to select which account
+            String accountType = (String) JOptionPane.showInputDialog(null, "Please select your account", "Select Account",
+                    JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice[0]);
+            //select what the user would like to do with the account?
+            //savings account
+            if(accountType == accountChoice[0]){
+                String webBasedChoice = (String) JOptionPane.showInputDialog(null, "Would you like to set withdraw limit or transfer funds to another account or calculate interest?",
+                        "Select Option", JOptionPane.QUESTION_MESSAGE, null, webBasedOption, webBasedOption[0]);
+                //if choice is set withdraw limit         
+                if(webBasedChoice == webBasedOption[0]){
+                    //ask user to enter withdraw limit
+                    double withdrawLimit = Double.parseDouble(JOptionPane.showInputDialog("Please enter the new withdraw limit"));
+                    //set new withdraw limit
+                    savingsAccount.setWithdrawLimit(withdrawLimit);
+                }
+                //if choice is to transfer funds
+                else if(webBasedChoice == webBasedOption[1]){
+                    String account = (String) JOptionPane.showInputDialog(null, "Which account do you want to transfer funds to?",
+                        "Select option", JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice [1]);
+                    //if trying to transfer to same account, throw error
+                    if(account == accountChoice[0]){
+                        JOptionPane.showMessageDialog(null, "You cannot transfer to the same account");
+                    }
+                    //net savings account
+                    else if(account == accountChoice[1]){
+                        transferBalance(savingsAccount, netSavingsAccount);
+                    }
+                    //cheque account
+                    else if(account == accountChoice[2]){
+                        transferBalance(savingsAccount, chequeAccount);
+                    }
+                    //fixed account
+                    else if (account == accountChoice[3]){
+                        transferBalance(savingsAccount, fixedAccount);
+                    } 
+                }
+                //if choice is calculate interest earned
+                else if (webBasedChoice == webBasedOption[2]){
+                    //calculate savings account interest
+                    savingsAccount.calculateInterestEarned();
+                }
+            }
+            //if account choice is net savings account
+            else if(accountType == accountChoice[1]){
+                //
+                String webBasedChoice = (String) JOptionPane.showInputDialog(null, "transfer funds to another account or calculate interest?",
+                        "Select Option", JOptionPane.QUESTION_MESSAGE, null, webBasedOption, webBasedOption[0]);
+                //net savings account cant set withdraw limit, so give error message
+                if(webBasedChoice == webBasedOption[0]){
+                    JOptionPane.showMessageDialog(null, "You cannot set withdraw limit for net savings account");
+                }
+                else if(webBasedChoice == webBasedOption[1]){
+                //ask user which account to send funds to
+                String account = (String) JOptionPane.showInputDialog(null, "Which account do you want to transfer funds to?",
+                        "Select option", JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice [0]);
+                    //if account to transfer to is savings account
+                    if(account == accountChoice[0]){
+                       transferBalance(netSavingsAccount, savingsAccount); 
+                    }
+                    // if same account chosen then send error
+                    else if(account == accountChoice[1]){
+                        JOptionPane.showMessageDialog(null, "You cannot transfer to the same account");
+                    }
+                    //if account = cheque account
+                    else if(account == accountChoice[2]){
+                        transferBalance(netSavingsAccount, chequeAccount);
+                    }
+                    //fixed account
+                    else if (account == accountChoice[3]){
+                        transferBalance(savingsAccount, fixedAccount);
+                    } 
+                }
+                //else calculate interest
+                else 
+                    netSavingsAccount.calculateInterestEarned();
+            }
+            //cheque account
+            else if (accountType == accountChoice[2]){
+                String webBasedChoice = (String) JOptionPane.showInputDialog(null, "transfer funds to another account or calculate interest?",
+                        "Select Option", JOptionPane.QUESTION_MESSAGE, null, webBasedOption, webBasedOption[0]);
+                if(webBasedChoice == webBasedOption[0]){
+                    JOptionPane.showMessageDialog(null, "You cannot set withdraw limit for cheque account");
+                }
+                if(webBasedChoice == webBasedOption[1]){
+                String account = (String) JOptionPane.showInputDialog(null, "Which account do you want to transfer funds to?",
+                        "Select option", JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice [0]);
+                    if(account == accountChoice[0]){
+                       transferBalance(chequeAccount, savingsAccount); 
+                    }
+                    else if(account == accountChoice[1]){
+                       transferBalance(chequeAccount, netSavingsAccount);
+                    }
+                    else if(account == accountChoice[2]){
+                        JOptionPane.showMessageDialog(null, "You cannot transfer to the same account");
+                    }
+                    else if (account == accountChoice[3]){
+                        transferBalance(chequeAccount, fixedAccount);
+                    } 
+                }
+                if(webBasedChoice == webBasedOption[2]){
+                    JOptionPane.showMessageDialog(null, "cheque account has no interest");
+                }
+            }
+            else if(accountType == accountChoice[3]){
+                
+                String webBasedChoice = (String) JOptionPane.showInputDialog(null, "transfer funds to another account or calculate interest?",
+                        "Select Option", JOptionPane.QUESTION_MESSAGE, null, webBasedOption, webBasedOption[0]);
+                
+                String account = (String) JOptionPane.showInputDialog(null, "Which account do you want to transfer funds to?",
+                        "Select option", JOptionPane.QUESTION_MESSAGE, null, accountChoice, accountChoice [0]);
+                
+                if(webBasedChoice == webBasedOption[0]){
+                    JOptionPane.showMessageDialog(null, "Fixed account doesn't offer a withdraw limit");
+                }
+                else if(webBasedChoice == webBasedOption[1]){
+                    if(account == accountChoice[0]){
+                       transferBalance(fixedAccount, savingsAccount); 
+                    }
+                    else if(account == accountChoice[1]){
+                       transferBalance(fixedAccount, netSavingsAccount);
+                    }
+                    else if(account == accountChoice[2]){
+                       transferBalance(fixedAccount, chequeAccount);
+                    }
+                    else if (account == accountChoice[3]){
+                        JOptionPane.showMessageDialog(null, "You cannot transfer to the same account");
+                    } 
+                }
+                else
+                    fixedAccount.calculateInterestEarned();
+            }
+    }
    
 }
